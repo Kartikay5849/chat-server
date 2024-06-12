163ed1b72ac1f86cbb7ab35fc766254c2bc504de
kartikay <104310254+Kartikay5849@users.noreply.github.com>
2024-06-12 07:17:30 +0530

    Update config.js

--- constants/config.js
+++ constants/config.js
@@ -1,12 +1,11 @@
 const corsOptions = {
   origin: [
-    'http://localhost:5173',
-    'http://localhost:4173',
-    'https://chat-client-lyart.vercel.app' // your Vercel frontend URL
+    "http://localhost:5173",
+    "http://localhost:4173",
+    'https://chat-client-lyart.vercel.app',
   ],
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true // allow sending cookies
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
 };
 
 const CHATTU_TOKEN = "chattu-token";
